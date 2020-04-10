ce5482c87b5574f9814c83b5b37961981f08065d
Barasakar <jlinpoughkeepsieday@gmail.com>
2020-04-09 22:39:05 -0500

    First commit

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+
+namespace Interactive360 {
+
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager instance = null;
+
+        Scene scene;
+        VideoPlayer video;
+        Animator anim;
+        Image fadeImage;
+
+        AsyncOperation operation;
+
+        [Header("Scene Management")]
+        public string[] scenesToLoad;
+        public string activeScene;
+
+        [Space]
+        [Header("UI Settings")]
+
+        public bool useFade;
+        public GameObject fadeOverlay;
+        public GameObject ControlUI;
+        public GameObject LoadingUI;
+
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                //DontDestroyOnLoad(gameObject);
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        void Start()
+        {
+            scene = SceneManager.GetActiveScene();
+            activeScene = scene.buildIndex + " - " + scene.name;
+        }
+
+        public void SelectScene(string sceneToLoad)
+        {
+            if(useFade)
+            {
+                StartCoroutine(FadeOutAndIn(sceneToLoad));
+            }
+            else
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+            //set the active scene to the next scene to load;
+            activeScene = sceneToLoad;
+        }
+
+        IEnumerator FadeOutAndIn(string sceneToload)
+        {
+            //get references to animatr and image component
+            anim = fadeOverlay.GetComponent<Animator>();
+            fadeImage = fadeOverlay.GetComponent<Image>();
+
+            //turn control UI off and loading UI on;
+            ControlUI.SetActive(false);
+            LoadingUI.SetActive(true);
+
+            //set FadeOut to true on the animator so our image will fade out
+            anim.SetBool("FadeOut", true);
+
+            yield return new WaitUntil(() => fadeImage.color.a == 1);
+            SceneManager.LoadScene(sceneToload);
+            Scene scene = SceneManager.GetSceneByName(sceneToload);
+            Debug.Log("loading scene: " + scene.name);
+            yield return new WaitUntil(() => scene.isLoaded);
+
+            video = FindObjectOfType<VideoPlayer>();
+            yield return new WaitUntil(() => video.isPrepared);
+
+            anim.SetBool("FadeOut", false);
+
+            yield return new WaitUntil(() => fadeImage.color.a == 0);
+            LoadingUI.SetActive(false);
+
+            if (ControlUI)
+            ControlUI.SetActive(true);
+     
+        }
+
+        public void Pausevideo()
+        { 
+            if(!video)
+            {
+                video = FindObjectOfType<VideoPlayer>();
+            }
+            video.Play();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Script/MenuManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Interactive360
+{
+    public class MenuManager : MonoBehaviour
+    {
+        //0 is bookstore
+        //1 is Kam;
+
+        public GameObject m_menu;
+        public Button[] m_buttonsInScene;
+        public GameObject m_playButton;
+        public GameObject m_pauseButton;
+        private AudioSource m_menuToggleAudio;
+       // [SerializeField] string m_oculusMenuToggle = "Button2";
+
+        void Start()
+        {
+            
+            BindButtonsToScenes();
+            m_menuToggleAudio = GetComponent<AudioSource>();
+        }
+        
+
+     
+        /*
+    void Update ()
+    {
+        checkForInput();
+    }
+    */
+    
+
+            
+        public void toggleMenu()
+        {
+            m_menu.SetActive(!m_menu.activeInHierarchy);
+        }
+        
+        /*
+        private void checkForInput()
+        {
+            if(Input.GetButtonDown(m_oculusMenuToggle))
+            {
+                toggleMenu();
+                if(m_menuToggleAudio)
+                {
+                    m_menuToggleAudio.Play();
+                }
+            }
+        }
+        */
+
+        public void toggleButton()
+        {
+            m_pauseButton.SetActive(!m_pauseButton.activeInHierarchy);
+            m_playButton.SetActive(!m_playButton.activeInHierarchy);
+        }
+
+        /*
+         Quit the application.
+         */
+        public void quit()
+        {
+            Application.Quit();
+        }
+
+        
+        public void BindButtonsToScenes()
+        {
+            if(m_buttonsInScene.Length != GameManager.instance.scenesToLoad.Length)
+            {
+                Debug.Log("Amount of buttons and scenes do not match!");
+                return;
+            } else
+            {
+                for (int i = 0; i < m_buttonsInScene.Length; i++)
+                {
+                    string sceneName = GameManager.instance.scenesToLoad[i];
+                    m_buttonsInScene[i].onClick.AddListener(() => GameManager.instance.SelectScene(sceneName));
+                }
+            }
+        }
+        
+
+    }
+}
+
