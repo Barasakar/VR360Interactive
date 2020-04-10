2aa2ab79992365f439b58b86a4660e1a2500d343
Barasakar <jlinpoughkeepsieday@gmail.com>
2020-04-10 09:41:15 -0500

    Commit

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -17,7 +17,7 @@ namespace Interactive360 {
         Image fadeImage;
 
         AsyncOperation operation;
-        //s
+        //hello ATLAS
         [Header("Scene Management")]
         public string[] scenesToLoad;
         public string activeScene;
